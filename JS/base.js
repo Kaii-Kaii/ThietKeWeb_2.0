c815327dbd7a5f7a4cc7b6349e9a14ee198e5b4d
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-04-24 16:51:11 +0700

    hi

--- JS/base.js
+++ JS/base.js
@@ -1,10 +1,10 @@
-function loadContent(pageName) {
+function loadContent(pageName, elementID) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("mainContent").innerHTML = this.responseText;
+            document.getElementById(elementID).innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "HTML/" + pageName + ".html", true); 
+    xhttp.open("GET", "HTML/" + pageName + ".html", true);
     xhttp.send();
 }
