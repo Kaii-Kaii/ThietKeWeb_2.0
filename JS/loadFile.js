bf34d6849f74a06bc947123cd04be062e76a2ab7
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-04-28 00:07:30 +0700

    fix something

--- JS/loadFile.js
+++ JS/loadFile.js
@@ -1,7 +1,16 @@
 $(function () {
     var includes = $('[data-include]');
+
     $.each(includes, function () {
-        var file = './HTML/' + $(this).data('include') + '.html';
-        $(this).load(file);
+        var file = 'HTML/' + $(this).data('include') + '.html';
+        var baseURL = window.location.href.replace(/\/[^\/]*$/, '/');
+
+        $(this).load(file, function() {
+            $(this).contents().each(function() {
+                if (this.nodeType === Node.ELEMENT_NODE) {
+                    $(this).attr('data-baseurl', baseURL);
+                }
+            });
+        });
     });
 });
