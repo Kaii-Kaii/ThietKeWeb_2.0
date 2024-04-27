60364e309ee2c884971e85162a4260b3766e2fc1
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-04-28 00:01:47 +0700

    fix bug

--- JS/loadFile.js
+++ JS/loadFile.js
@@ -1,7 +1,7 @@
 $(function () {
     var includes = $('[data-include]');
     $.each(includes, function () {
-        var file = 'HTML/' + $(this).data('include') + '.html';
+        var file = './HTML/' + $(this).data('include') + '.html';
         $(this).load(file);
     });
 });
