db961c2c7b3846551a08004f826cae2fa4275553
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-04-28 00:10:23 +0700

    fix

--- JS/loadFile.js
+++ JS/loadFile.js
@@ -1,10 +1,8 @@
 $(function () {
     var includes = $('[data-include]');
-
     $.each(includes, function () {
         var file = 'HTML/' + $(this).data('include') + '.html';
-        var baseURL = window.location.href.replace(/\/[^\/]*$/, '/');
-
+        var baseURL = includes + '.html';
         $(this).load(file, function() {
             $(this).contents().each(function() {
                 if (this.nodeType === Node.ELEMENT_NODE) {
