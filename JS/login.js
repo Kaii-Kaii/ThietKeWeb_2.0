9a8ea6aa78db568b8c4b54c156bf8920812669bb
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-05-21 23:05:33 +0700

    fix bug

--- JS/login.js
+++ JS/login.js
@@ -173,3 +173,49 @@ function handleSignup(event) {
     window.opener.postMessage({ username_dk: username_dk, password_dk: password_dk, ten: hovaTen, email: email, sdt: sdt }, '*');
     toggleForm('login');
 }
+
+
+function checkUsernameAvailability(username) {
+    return new Promise(resolve => {
+        const userExists = ds_taiKhoan.some(user => user.taiKhoan === username);
+        resolve(!userExists);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const emailInput = document.getElementById('email');
+    const emailWarning = document.getElementById('canh-bao-email');
+    const sdtInput = document.getElementById('sdt');
+    const sdtWarning = document.getElementById('canh-bao-sdt');
+    const passwordInput = document.getElementById('password_signup');
+    const passwordWarning = document.getElementById('canh-bao-password');
+    const repasswordInput = document.getElementById('repassword');
+    const repasswordWarning = document.getElementById('canh-bao-repassword');
+    const usernameInput = document.getElementById('username_signup');
+    const usernameWarning = document.getElementById('canh-bao-username');
+
+    emailInput.addEventListener('input', function() {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        emailWarning.style.display = emailPattern.test(emailInput.value) ? 'none' : 'block';
+    });
+
+    sdtInput.addEventListener('input', function() {
+        const sdtPattern = /^[0-9]{10}$/;
+        sdtWarning.style.display = sdtPattern.test(sdtInput.value) ? 'none' : 'block';
+    });
+
+    passwordInput.addEventListener('input', function() {
+        passwordWarning.style.display = passwordInput.value.length >= 8 ? 'none' : 'block';
+    });
+
+    repasswordInput.addEventListener('input', function() {
+        repasswordWarning.style.display = repasswordInput.value === passwordInput.value ? 'none' : 'block';
+        repasswordInput === '' ? repasswordWarning.style.display = 'none' : '';
+    });
+
+    usernameInput.addEventListener('input', function() {
+        checkUsernameAvailability(usernameInput.value).then(isAvailable => {
+            (usernameWarning.style.display = isAvailable) ? 'none' : 'block';
+        });
+    });
+});
