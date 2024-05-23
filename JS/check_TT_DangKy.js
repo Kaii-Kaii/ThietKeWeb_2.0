4922b24a36aa7a026b72992f2555dcf48e668ac1
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-05-23 16:14:32 +0700

    fix bug

--- JS/check_TT_DangKy.js
+++ JS/check_TT_DangKy.js
@@ -1,4 +1,4 @@
-// Kiểm tra thông tin đăng nhập
+//! Kiểm tra thông tin đăng ký
 document.addEventListener('DOMContentLoaded', function () {
     const emailInput = document.getElementById('email');
     const emailWarning = document.getElementById('canh-bao-email');
@@ -10,17 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const repasswordWarning = document.getElementById('canh-bao-repassword');
     const usernameInput = document.getElementById('username_signup');
     const usernameWarning = document.getElementById('canh-bao-username');
-
     emailInput.addEventListener('input', function () {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         emailWarning.style.display = emailPattern.test(emailInput.value) ? 'none' : 'block';
     });
-
     sdtInput.addEventListener('input', function () {
         const sdtPattern = /^[0-9]{10}$/;
         sdtWarning.style.display = sdtPattern.test(sdtInput.value) ? 'none' : 'block';
     });
-
     passwordInput.addEventListener('input', function () {
         passwordWarning.style.display = passwordInput.value.length >= 8 ? 'none' : 'block';
     });
